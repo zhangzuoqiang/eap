bdc6efa341f49a2983aa2c4f10dae5ec4f42f133
chiknin <chiknin@gmail.com>
2014-09-03 14:19:51 +0800

    【优化】修改getRemoteAdd方法，添加从请求头X-ClientIP中获取

--- eap.web/src/main/java/eap/util/HttpUtil.java
+++ eap.web/src/main/java/eap/util/HttpUtil.java
@@ -82,6 +82,9 @@ public class HttpUtil {
 	public static String getRemoteAddr(HttpServletRequest request) {
 		String ip = request.getHeader("x-forwarded-for");
 		if(StringUtils.isBlank(ip) || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("X-ClientIP");
+		}
+		if(StringUtils.isBlank(ip) || "unknown".equalsIgnoreCase(ip)) {
 			ip = request.getHeader("Proxy-Client-IP");
 		}
 		if(StringUtils.isBlank(ip) || "unknown".equalsIgnoreCase(ip)) {
